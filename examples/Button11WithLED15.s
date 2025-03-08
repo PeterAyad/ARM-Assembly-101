0104e7e295eb1041dcb3e731f9049bf6cecb05e0
PeterAyad <petermayad@hotmail.com>
2025-03-08 19:17:04 +0200

    edited comments

--- examples/Button11WithLED15.s
+++ examples/Button11WithLED15.s
@@ -19,50 +19,55 @@ GPIOD_IDR        EQU  0x40020C10
         
 __main FUNCTION
 
-    ; Enable GPIO-D clock
+    ; Enable GPIO clock
     LDR     R1, =RCC_AHB1ENR    
     LDR     R0, [R1]            
-    ORR     R0, R0, #0x08        ; Enable GPIOD clock
+    ORR     R0, R0, #0x08
     STR     R0, [R1]            
 
-    ; Configure pin 15 as output mode (bits 30-31 = 01)
+    ; Configure pin 15 as output mode
     LDR     R1, =GPIOD_MODER    
     LDR     R0, [R1]                 
-    BFC     R0, #30, #2          ; Clear bits 30-31
-    BFI     R0, R0, #30, #1      ; Insert '01' at bit 30
+    BFC     R0, #30, #2
+    BFI     R0, R0, #30, #1
     STR     R0, [R1]            
 
-    ; Configure pin 11 as input mode (bits 22-23 = 00)
+    ; Configure pin 11 as input mode
     LDR     R1, =GPIOD_MODER    
     LDR     R0, [R1]            
-    BFC     R0, #22, #2          ; Clear bits 22-23 (set to 00)
+    BFC     R0, #22, #2          
     STR     R0, [R1]            
 
-    ; Disable pull-up/pull-down resistors for pin 11 (bits 22-23 = 00)
+    ; Enable pulldown resistor on pin 11(Button)
     LDR     R1, =GPIOD_PUPDR    
     LDR     R0, [R1]
-    BFC     R0, #22, #2          ; Clear bits 22-23 (set to 00)
+    BFC     R0, #22, #2   
     STR     R0, [R1]
 
+; Loop
+
+    ;Read Button from Input Port
 readInput
     LDR     R1, =GPIOD_IDR     
     LDR     R0, [R1]
-    LSR     R0, R0, #11         ; Move bit 11 to bit 0
-    AND     R0, R0, #1          ; Mask bit 0
+    LSR     R0, R0, #11
+    AND     R0, R0, #1
     CMP     R0, #1             
     BNE     turnOff
 
+    ;Turn On LED using output port
 turnOn
     LDR     R1, =GPIOD_ODR      
     LDR     R0, [R1]
-    BFI     R0, R0, #15, #1     ; Set bit 15 (turn on LED)
+    BFI     R0, R0, #15, #1 
     STR     R0, [R1]
     B       readInput
 
+    ;Turn Off LED using output port
 turnOff
     LDR     R1, =GPIOD_ODR      
     LDR     R0, [R1]
-    BFC     R0, #15, #1         ; Clear bit 15 (turn off LED)
+    BFC     R0, #15, #1  
     STR     R0, [R1]
     B       readInput
     
